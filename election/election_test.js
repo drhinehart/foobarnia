64fc60904cbb61ea2cb7a0a75eed2f3b81842f3e
drhinehart <drhinehart@gmail.com>
2019-03-04 22:55:01 -0600

    adding some test to check that initial count array is created correctly, populated counts array is created, and that runoff tie function is working properly

--- election/election_test.js
+++ election/election_test.js
@@ -23,3 +23,28 @@ test('election runoff with tie', function(t) {
   results.sort( function(a,b) { a - b } );
   t.same(results, [1,2,3,4,5]);
 });
+
+test('initial counts array', function(t) {
+  t.plan(1);
+  var votes = [2,2,5,5,5,7,5,5,1,5,2];
+  var results = election.buildInitialCountsArray(votes);
+  t.same(results, [0,0,0,0,0,0,0,0]);
+});
+
+test('build counts array', function(t) {
+  t.plan(1);
+  var votes = [2,2,5,5,5,7,5,5,1,5,2];
+  var counts = election.buildInitialCountsArray(votes);
+  var results = election.buildCounts(votes, counts);
+  t.same(results, [0,1,3,0,0,6,0,1]);
+});
+
+test('get runoff array', function(t) {
+  t.plan(1);
+  var votes = [5,5,5,2,2,1,1,3,3,6,9,4];
+  var counts = election.buildInitialCountsArray(votes);
+  counts = election.buildCounts(votes, counts);
+  var results = election.findRunoffCandidates(counts);
+  results.sort( function(a,b) { a - b } );
+  t.same(results, [1,2,3,5]);
+});
